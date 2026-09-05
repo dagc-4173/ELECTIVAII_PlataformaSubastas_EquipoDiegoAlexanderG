4d70fca1ed98503e438b462a1b9410cfee6b6ade
dagc-4173 <dagc-4173@hotmail.com>
2026-09-05 05:00:52 -0500

    chore: configure server port from environment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./infrastructure/app.js";
 
-const port = 3000;
+const port = Number(process.env.PORT ?? 3000);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
